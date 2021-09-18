a1b5e97369cfc44145d621762ec36550725a52fd
thanaphon28 <thanaphon.je@ku.th>
2021-09-18 20:01:09 +0700

    first commit

---
+++ HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+
+
+<div id = "hello">
+  
+  <h1>  {{myName}} </h1>
+  <p> Tel : {{tel}} </p>
+  <p> {{no}} </p>
+
+
+  <button>click me</button>
+  <div class="btn-group" role="group" aria-label="Basic example">
+  <button type="button" class="btn btn-primary">Left</button>
+  <button type="button" class="btn btn-primary">Middle</button>
+  <button type="button" class="btn btn-primary">Right</button>
+  <i class="far fa-address-card"></i>
+</div>
+</div>
+  
+</template>
+
+<script>
+export default {
+  name : "hello", 
+data() {
+  return {
+    myName : "Thanaphon Jedsadalak",
+    tel: "0657401212",
+    no : "6130203209"
+  };
+}
+}
+</script>
+
+<style>
+.test{
+  font-size:  9em;
+}
+#hello{
+  background-color: red;
+  color: white;
+}
+
+
+</style>
